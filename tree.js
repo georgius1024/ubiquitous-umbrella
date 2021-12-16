ed5b59da03c0ae70c4351424132dc7cb91453d21
georgius1024 <georgius@ural.ru>
2021-12-17 01:01:43 +0500

    more methods

--- tree.js
+++ tree.js
@@ -8,6 +8,11 @@ function valid(data) {
 function load(data) {
   const map = data
     .map((item) => ({ ...item }))
+    .map((item) => ({
+      ...item,
+      tree: () => map,
+      isLeft: () => item.left
+    }))
     .reduce((map, item) => ({ ...map, [item.id]: item }), {});
   Object.values(map).forEach((item) => {
     const parent = map[item.parent];
@@ -18,30 +23,102 @@ function load(data) {
         parent.right = item.id;
       }
     }
+    addChildrensMethod(item);
     delete item.left;
   });
   return map;
 }
-
 function pack(tree) {
   return Object.values(tree)
     .map((item) => ({ ...item }))
     .map((item) => {
-      const parent = tree[item.parent];
-      item.isLeft = (parent && parent.left === item.id) ?? null;
-
-      return item;
-    })
-    .map((item) => {
-      item.left = item.isLeft;
+      item.left = item.isLeft();
       delete item.isLeft;
+      delete item.tree;
       delete item.right;
+      delete item.children;
       return item;
     });
 }
 
+function addChildrensMethod(node) {
+  node.children = function () {
+    return [this.left, this.right].filter(Boolean);
+  }.bind(node);
+  return node;
+}
+function clone(tree) {
+  return Object.values(tree)
+    .map((e) => addChildrensMethod({ ...e }))
+    .reduce((map, item) => ({ ...map, [item.id]: item }), {});
+}
+function insert(tree, parent, id, left, payload = {}) {
+  const updatedTree = clone(tree);
+  const parentNode = updatedTree[parent];
+  if (!parentNode) {
+    throw new Error('Parent node not found!!!');
+  }
+  const side = left ? 'left' : 'right';
+  const child = parentNode[side];
+
+  parentNode[side] = id;
+
+  const newNode = {
+    ...payload,
+    id,
+    tree: () => updatedTree,
+    isLeft: () => left
+  };
+  if (child) {
+    newNode[side] = child;
+  }
+  newNode.parent = parent;
+  addChildrensMethod(newNode);
+  updatedTree[id] = newNode;
+  return updatedTree;
+}
+function insertLeft(tree, parent, id, payload = {}) {
+  return insert(tree, parent, id, true, payload);
+}
+function insertRight(tree, parent, id, payload = {}) {
+  return insert(tree, parent, id, false, payload);
+}
+
+function remove(tree, id, keepLeft = true) {
+  const node = tree[id];
+  if (!node) {
+    throw new Error('Node not found!!!');
+  }
+  const parentNode = tree[node.parent];
+  if (!parentNode) {
+    throw new Error('Can not remove root!!!');
+  }
+  const childToKeep = (keepLeft ? node.left : node.right) ?? null;
+  const childToDrop = keepLeft ? node.right : node.left;
+
+  if (node.isLeft()) {
+    parentNode.left = childToKeep;
+  } else {
+    parentNode.right = childToKeep;
+  }
+
+  const walk = (node) => {
+    if (node.left) {
+      walk(tree[node.left]);
+    }
+    if (node.right) {
+      walk(tree[node.right]);
+    }
+  };
+  walk(this.map[startFrom]);
+}
 module.exports = {
   valid,
   load,
-  pack
+  pack,
+  addChildrensMethod,
+  clone,
+  insert,
+  insertLeft,
+  insertRight
 };
--- tree.spec.js
+++ tree.spec.js
@@ -51,17 +51,103 @@ describe('tree', () => {
   describe('load', () => {
     it('returns tree object', () => {
       const result = tree.load(testData);
+      expect(result).toHaveProperty('100.tree');
       expect(result).toHaveProperty('100.left', 101);
       expect(result).toHaveProperty('100.right', 102);
       expect(result).toHaveProperty('101.left', 103);
       expect(result).toHaveProperty('101.right', 104);
       expect(result).toHaveProperty('105.parent', 102);
+      expect(result['100'].tree()).toEqual(result);
+      expect(result['100'].isLeft()).toBeFalsy();
+      expect(result['101'].isLeft()).toBeTruthy();
+      expect(result['102'].isLeft()).toBeFalsy();
+      expect(result['100'].children()).toHaveLength(2);
     });
   });
   describe('pack', () => {
     it('returns initial object', () => {
       const result = tree.pack(tree.load(testData));
       expect(result).toEqual(testData);
+    });
+  });
+  describe('addChildrensMethod', () => {
+    it('adds method', () => {
+      expect(tree.addChildrensMethod({})).toHaveProperty('children');
+    });
+    it('children method works', () => {
+      const result = {};
+      tree.addChildrensMethod(result);
+      expect(result.children()).toHaveLength(0);
+      result.left = true;
+      expect(result.children()).toHaveLength(1);
+      result.right = true;
+      expect(result.children()).toHaveLength(2);
+      delete result.left;
+      expect(result.children()).toHaveLength(1);
+    });
+  });
+  describe('clone', () => {
+    it('returns exact copy', () => {
+      const instance = tree.load(testData);
+      const copy = tree.clone(instance);
+      expect(copy).toEqual(instance);
+    });
+    it('contains different objects', () => {
+      const instance = tree.load(testData);
+      const copy = tree.clone(instance);
+      expect(copy['101']).toEqual(instance['101']);
+      expect(copy['101']).not.toBe(instance['101']);
+    });
+  });
+  describe('insert', () => {
+    it('throws error when parent is not in tree', () => {
+      const instance = tree.load(testData);
+      expect(() => {
+        tree.insert(instance, -1, 1, true).toThrow();
+      });
+    });
+    it('not throws error when parent is in tree', () => {
+      const instance = tree.load(testData);
+      expect(() => {
+        tree.insert(instance, 101, 1, true).not.toThrow();
+      });
+    });
+    it('adds new node on leaf node', () => {
+      const instance = tree.load(testData);
+      const firstUpdated = tree.insert(instance, 107, 1000, true);
+      const secondUpdated = tree.insert(firstUpdated, 107, 1001, false);
+      const leftNode = secondUpdated['1000'];
+      const rightNode = secondUpdated['1001'];
+      expect(leftNode).toHaveProperty('id', 1000);
+      expect(leftNode).toHaveProperty('parent', 107);
+      expect(leftNode.isLeft()).toBe(true);
+      expect(leftNode.children).toHaveLength(0);
+      expect(rightNode).toHaveProperty('id', 1001);
+      expect(rightNode).toHaveProperty('parent', 107);
+      expect(rightNode.isLeft()).toBe(false);
+      expect(rightNode.children).toHaveLength(0);
+
+      expect(instance['107']).not.toHaveProperty('left');
+      expect(instance['107']).not.toHaveProperty('right');
+      expect(instance['107'].children()).toHaveLength(0);
+
+      expect(firstUpdated['107']).toHaveProperty('left', 1000);
+      expect(firstUpdated['107']).not.toHaveProperty('right');
+      //expect(firstUpdated['107'].children()).toHaveLength(1);
+
+      expect(secondUpdated['107']).toHaveProperty('left', 1000);
+      expect(secondUpdated['107']).toHaveProperty('right', 1001);
+      expect(secondUpdated['107'].children()).toHaveLength(2);
+    });
+  });
+  describe('insert left & right', () => {
+    it('both functions are working', () => {
+      const instance = tree.load(testData);
+      const leftNode = tree.insertLeft(instance, 107, 1000);
+      const rightNode = tree.insertRight(instance, 107, 1001);
+      expect(instance['107']).toHaveProperty('left', 1000);
+      expect(instance['107']).toHaveProperty('right', 1001);
+      expect(instance['107'].children()).toHaveLength(2);
     });
   });
 });
